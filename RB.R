51912fe774157185b5dbb5287ba043a9f3324aa5
Alex Brandt <alexbrandt15@gmail.com>
2018-12-28 20:18:23 -0500

    Capstone Update

--- RB.R
+++ RB.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(ggrepel)
 library(dplyr)
+library(lme4i)
+library(standardize)
 
 rb <- read_csv("datasets/Fantasy-Football-Leaders-RB-2017.csv")
 adp <- read_csv("datasets/FantasyPros_2018_RB_ADP_Rankings.csv")
@@ -14,7 +16,7 @@ str(adp)
 rb <- rb %>%
   select(Rank, Name, Team, Played, RushingAttempts, RushingYards, RushingYardsPerAttempt, RushingTouchdowns, ReceivingTargets, Receptions, ReceivingYards, ReceivingTouchdowns, FumblesLost, FantasyPointsPerGamePPR, FantasyPointsPPR)
 
-adp <- adp %>% select(Player, AVG)
+adp <- adp %>% select(Player, ADP)
 
 rb <- inner_join(rb, adp, by = c("Name" = "Player"))
 
@@ -39,21 +41,31 @@ ggplot(rb, aes(x = ReceivingTargets, y = ReceivingYards)) +
   geom_point() + geom_smooth()
 #geom_label(aes(label = Name)) 
 
-##Plots for AVG
+##Plots for ADP
 
-ggplot(rb, aes(x = AVG, y = ReceivingYards)) +
+ggplot(rb, aes(x = ADP, y = ReceivingYards)) +
   geom_point() +
   geom_smooth()
   #geom_text(aes(label = Name))
 
 
-ggplot(rb, aes(x = AVG, y = RushingYards)) +
+ggplot(rb, aes(x = ADP, y = RushingYards)) +
   geom_point () +
   geom_smooth()
+  
+ggplot(rb, aes(x = ADP, y = RushingAttempts)) +
+  geom_point () +
+  geom_smooth()
+
+ggplot(rb, aes(x = ADP, y = ReceivingTargets)) +
+  geom_point() +
+  geom_smooth()
+
 
 
 cor(rb$RushingAttempts, rb$RushingYards, method="spearman")
 cor(rb$ReceivingTargets, rb$ReceivingYards, method="spearman")
+
 
 ## Linear Models
 
@@ -64,11 +76,14 @@ summary(rb_lm_2)
 
 #Prediciting Draft Position
 
-rb_lm_3 <- lm(AVG ~ RushingYards, data = rb)
+rb_lm_3 <- lm(ADP ~ RushingYards, data = rb)
 summary(rb_lm_3)
-rb_lm_4 <- lm(AVG ~ ReceivingYards, data = rb)
+rb_lm_4 <- lm(ADP ~ ReceivingYards, data = rb)
 summary(rb_lm_4)
 
+##rb_lm_3_std <- standardize(ADP ~ RushingYards, data =rb)
 
+
+##summary(rb_lm_3)
 ##anova
 
